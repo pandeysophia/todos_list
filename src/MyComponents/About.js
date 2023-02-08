4b8c410404de7b8d9f3edefa43f5d38b04d99412
Sophia <74519162+pandeysophia@users.noreply.github.com>
2023-02-08 15:38:16 -0600

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        About
+        <p>This is a react project done by Sophia on 02/07/2023. 
+            Learning react and trying to find my first job as a developer.
+        </p>
+    </div>
+  )
+}
+
